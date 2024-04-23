e65a6704448014db0d434a7357dbbdb3f29c72a3
unknown <lukestogsdill@gmail.com>
2024-04-23 03:48:18 -0500

    client now display lmm stream

--- banana-peel/src/components/Form.jsx
+++ banana-peel/src/components/Form.jsx
@@ -1,14 +1,47 @@
 import React from "react";
-import fetchLMM from "../utils/fetchLMM";
-import './form.css'
+import "./form.css";
 
 const Form = ({ form, setForm, setTextResponse }) => {
+  async function fetchLMM(prompt) {
+    try {
+      const payload = {
+        prompt: prompt,
+      };
+
+      const response = await fetch("http://localhost:5000/kobold/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}`);
+      }
+
+      // Read the stream
+      const reader = response.body.getReader();
+      const decoder = new TextDecoder();
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const chunk = decoder.decode(value);
+        const dataLine = chunk.split("\n")[1];
+        const jsonPart = dataLine.slice(5);
+        const object = JSON.parse(jsonPart);
+        console.log(object.token);
+        setTextResponse((prev) => prev + object.token);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const prompt = e.target.elements.prompt.value;
-    const response = await fetchLMM(prompt);
-    setTextResponse(response);
+    fetchLMM(prompt);
   };
 
   const handleChange = (e) => {
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
