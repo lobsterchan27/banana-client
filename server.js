9a5921973a22d3fed683892140049e25a76d5995
lobsterchan27 <lobbypolice@gmail.com>
2024-04-25 03:48:08 -0400

    chat endpoint router

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use('/kobold', require('./src/kobold').router);
 
 app.use('/banana', require('./src/banana').router);
 
+app.use('/chat', require('./src/chat').router);
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
