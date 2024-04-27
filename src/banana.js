a8bc06123c9e8b93cfcc6d019ee33d97bc33b8c4
lobsterchan27 <lobbypolice@gmail.com>
2024-04-27 03:19:57 -0400

    factor fetchTTS. save file with unique id and returns file path

--- src/banana.js
+++ src/banana.js
@@ -1,41 +1,47 @@
 const express = require('express');
 const fetch = require('node-fetch').default;
-const { Readable } = require('stream');
 const fs = require('fs');
 
 const router = express.Router();
 
+async function fetchTTS(url, payload) {
+    const fetchResponse = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    const data = await fetchResponse.buffer();
+    const filepath = path.join(__dirname, 'public', `audio_${Date.now()}.wav`)
+
+    fs.writeFile(filepath, data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+
+    return filepath;
+}
+
 router.post('/text2speech', async function (request, response) {
-    if (!request.body) return response_generate.sendStatus(400);
+    if (!request.body) return response.sendStatus(400);
 
     if (request.body.api_server.indexOf('localhost') != -1) {
         request.body.api_server = request.body.api_server.replace('localhost', '127.0.0.1');
     }
     
-    const url = 'http://localhost:8127/api/text2speech'
+    const url = request.body.api_server + '/text2speech';
     const payload = {
-        'prompt': 'machi is so cool. shiko shiko lick lick',
-        'voice': 'reference'
+        'prompt': request.body.prompt,
+        'voice': request.body.voice || 'reference'
     }
+
     try {
-        const fetchResponse = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(payload),
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-        const data = await fetchResponse.buffer();
-
-        fs.writeFile('network.wav', data, (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-        });
-
-        response.status(200).send('Transcription complete')
+        const filename = await fetchTTS(url, payload);
+        response.status(200).json({ message: 'Transcription complete', filename: filename });
     } catch (error) {
         console.error('Error:', error);
         response.status(500).send('An error occurred');
     }
 });
 
-module.exports = { router };
+module.exports = { router, fetchTTS};
