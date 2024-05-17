28099a2cf22bd54911631ea46e36f0fd7bf6de16
lobsterchan27 <lobbypolice@gmail.com>
2024-05-17 04:36:28 -0400

    settings constants

---
+++ settings.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const PROJECT_ROOT = __dirname;
+const YT_DLP_BINARY_PATH = path.join(PROJECT_ROOT, 'bin');
+
+module.exports = {
+    PROJECT_ROOT,
+    YT_DLP_BINARY_PATH,
+    };
