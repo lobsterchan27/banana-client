5f6551a2f99088fd68c2742dacab61bd435ea97b
lobsterchan27 <lobbypolice@gmail.com>
2024-05-22 18:57:50 -0400

    api key

--- settings.js
+++ settings.js
@@ -3,7 +3,10 @@ const path = require('path');
 const PROJECT_ROOT = __dirname;
 const YT_DLP_BINARY_PATH = path.join(PROJECT_ROOT, 'bin');
 
+const API_KEY = process.env.API_KEY
+
 module.exports = {
     PROJECT_ROOT,
     YT_DLP_BINARY_PATH,
+    API_KEY,
     };
