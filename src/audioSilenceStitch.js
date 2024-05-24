925d4279dd1952a367ab2eb3aeb60226b062ebe8
unknown <lukestogsdill@gmail.com>
2024-05-24 06:53:34 -0500

    made backup

--- src/audioSilenceStitch.js
+++ src/audioSilenceStitch.js
@@ -3,78 +3,79 @@ const wavFileInfo = require("wav-file-info");
 const { loadJson, saveJson } = require("./utils");
 const { exec } = require('child_process');
 const { PROJECT_ROOT } = require("../settings");
+const fs = require('fs');
 
 async function audioSilenceStitch(contextName) {
+  const contextPath = path.join(PROJECT_ROOT, "public", "context", contextName);
+  const jsonPath = path.join(contextPath, `${contextName}.json`);
+  const contextChunks = await loadJson(jsonPath);
+  const outputFile = path.join(contextPath, `${contextName}_final_output.wav`);
 
-  try {
-    const contextPath = path.join(PROJECT_ROOT, "public", "context", contextName);
-    const jsonPath = path.join(contextPath, `${contextName}.json`);
-    const contextChunks = await loadJson(jsonPath);
-    const outputFile = path.join(contextPath, `${contextName}_final_output.wav`);
-  
-    let prevEntryTime = 0;
-    let prevInfoDuration = 0;
-    let ffmpegInputs = [];
-    let concatFilter = [];
-    let streamIndex = 0;
-    let silenceDurations = [];
-  
-    // creating ffmpeg command
-    for (const [key, segment] of Object.entries(contextChunks)) {
-      const entryTime = segment.segments[segment.segments.length - 1].end;
-      const currentAudioFilePath = path.join(contextPath, `${contextName}_${key}.wav`);
-      let info = await getAudioInfo(currentAudioFilePath);
-      const silenceDuration = Math.max(0, (entryTime - prevEntryTime - prevInfoDuration).toFixed(3));
-      silenceDurations.push(silenceDuration);
-  
-      // Prepare FFmpeg command parts
-      if (silenceDuration > 0) {
-        ffmpegInputs.push(`-f lavfi -t ${silenceDuration} -i anullsrc=r=48000:cl=mono`);
-        concatFilter.push(`[${streamIndex}:a]`);
-        streamIndex++;
-      }
-      ffmpegInputs.push(`-i "${currentAudioFilePath}"`);
+  let prevEntryTime = 0;
+  let prevInfoDuration = 0;
+  let ffmpegInputs = [];
+  let concatFilter = [];
+  let streamIndex = 0;
+  let silenceDurations = [];
+
+  // creating ffmpeg command
+  for (const [key, segment] of Object.entries(contextChunks)) {
+    const entryTime = segment.segments[segment.segments.length - 1].end;
+    const currentAudioFilePath = path.join(contextPath, `${contextName}_${key}.wav`);
+    let info = await getAudioInfo(currentAudioFilePath);
+    const silenceDuration = Math.max(0, (entryTime - prevEntryTime - prevInfoDuration).toFixed(3));
+    silenceDurations.push(silenceDuration);
+
+    // Prepare FFmpeg command parts
+    if (silenceDuration > 0) {
+      ffmpegInputs.push(`-f lavfi -t ${silenceDuration} -i anullsrc=r=48000:cl=mono`);
       concatFilter.push(`[${streamIndex}:a]`);
       streamIndex++;
-  
-      // Update variables for next iteration
-      prevInfoDuration = info.duration;
-      prevEntryTime = entryTime;
     }
-  
-    // update subs times
-    let endTime = 0;
-    const firstSubStart = Object.values(contextChunks)[0].subs[0].start;
-    console.log("firstSubStart", firstSubStart)
-    console.log("silenceDurations", silenceDurations[0])
-    if (firstSubStart < silenceDurations[0]) {
-      console.log("updating sub times")
-      for (const [key, segment] of Object.entries(contextChunks)) {
-        const prevEndTime = updateSubTimes(segment.subs, silenceDurations[key], endTime);
-        console.log(prevEndTime)
-        endTime = prevEndTime;  
-      }
-    } else {
-      console.log("subs already updated")
+    ffmpegInputs.push(`-i "${currentAudioFilePath}"`);
+    concatFilter.push(`[${streamIndex}:a]`);
+    streamIndex++;
+
+    // Update variables for next iteration
+    prevInfoDuration = info.duration;
+    prevEntryTime = entryTime;
+  }
+
+  // update subs times
+  let endTime = 0;
+  const firstSubStart = Object.values(contextChunks)[0].subs[0].start;
+  console.log("firstSubStart", firstSubStart)
+  console.log("silenceDurations", silenceDurations[0])
+  if (firstSubStart < silenceDurations[0]) {
+    console.log("updating sub times")
+
+    // Backup the original JSON file
+    const backupPath = path.join(path.dirname(jsonPath), 'backup.json');
+    fs.copyFileSync(jsonPath, backupPath);
+    
+    for (const [key, segment] of Object.entries(contextChunks)) {
+      const prevEndTime = updateSubTimes(segment.subs, silenceDurations[key], endTime);
+      console.log(prevEndTime)
+      endTime = prevEndTime;  
     }
     // Save the updated contextChunks back to the JSON file
     await saveJson(jsonPath, contextChunks);
-  
-    // Build and execute FFmpeg command
-    let filterComplex = `${concatFilter.join("")}concat=n=${concatFilter.length}:v=0:a=1`;
-    let ffmpegCmd = `ffmpeg ${ffmpegInputs.join(" ")} -filter_complex "${filterComplex}" "${outputFile}"`;
-    console.log("FFmpeg command:", ffmpegCmd);
-  
-    exec(ffmpegCmd, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing FFmpeg: ${error}`);
-        return;
-      }
-      console.log("FFmpeg operation completed successfully.");
-    });
-  } catch (error) {
-    console.error("Error stitching audio segments:", error);
+  } else {
+    console.log("subs already updated")
   }
+
+  // Build and execute FFmpeg command
+  let filterComplex = `${concatFilter.join("")}concat=n=${concatFilter.length}:v=0:a=1`;
+  let ffmpegCmd = `ffmpeg ${ffmpegInputs.join(" ")} -filter_complex "${filterComplex}" "${outputFile}"`;
+  console.log("FFmpeg command:", ffmpegCmd);
+
+  exec(ffmpegCmd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing FFmpeg: ${error}`);
+      return;
+    }
+    console.log("FFmpeg operation completed successfully.");
+  });
 }
 
 function updateSubTimes(subs, silenceDuration, endTime) {
